5f6b0145d8e6adaf40fd2a0c57599f604bec2b79
zainabzafar23 <zainabzafarkhan23@gmail.com>
2024-08-10 19:28:58 +0500

    commit

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,33 @@
+function submitFeedback(){
+    const name=document.getElementById('name').value;
+    const age =document.getElementById('age').value;
+    const email=document.getElementById('email').value;
+    const job=document.getElementById('job').value;
+    const designation=document.getElementById('designation').value;
+    const productType=document.getElementById('productType').value;
+    const feedback=document.getElementById('feedbackText').value;
+    const userexp=document.getElementById('userExperience').value;
+    
+    alert('thankyou for your valuable feedback')
+
+
+    document.getElementById('userName').innerHTML = name;
+    document.getElementById('userAge').innerHTML = age;
+    document.getElementById('userEmail').innerHTML = email;
+    document.getElementById('userJob').innerHTML = job;
+    document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userexp;
+
+    document.getElementById('userInfo').style.display = 'block';
+}
+
+const submitButton=document.getElementById('submitBtn');
+submitButton.onclick=submitFeedback;
+
+document.addEventListener('keydown',function(event) {
+    if (event.key =='Enter') { 
+    submitFeedback();
+}
+});
